96b5f4acd3a1c3d310012c863c6d5a70a2d70a46
Atulaman <atul.aman@remotestate.com>
2024-05-06 17:43:27 +0530

    changed ACI.tf file

--- ACR/ACI.tf
+++ ACR/ACI.tf
@@ -1,13 +1,15 @@
 # Create an Azure Container Instance
-resource "azurerm_container_group" "my_app" {
-  name                = "my-container-group"
+resource "azurerm_container_group" "example" {
+  name                = "example-continst"
   location            = azurerm_resource_group.react.location
   resource_group_name = azurerm_resource_group.react.name
+  ip_address_type     = "Public"
+  //dns_name_label      = "aci-label"
   os_type             = "Linux"
 
   container {
-    name   = "my-container"
-    image  = "mikku1999.azurecr.io/react"  # Replace with your image reference
+    name   = "react-app"
+    image  = "mikku1999.azurecr.io/react"
     cpu    = "0.5"
     memory = "1.5"
 
@@ -15,13 +17,5 @@ resource "azurerm_container_group" "my_app" {
       port     = 8080
       protocol = "TCP"
     }
-
-    environment_variables = {
-      # Define environment variables if needed
-    }
-  }
-
-  tags = {
-    environment = "Devlopment"
   }
 }
