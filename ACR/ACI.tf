052ac16b14ad75ce6d85a99476c1bd7d9915d15d
Atulaman <atul.aman@remotestate.com>
2024-05-06 17:16:02 +0530

    modified ACI.tf file

--- ACR/ACI.tf
+++ ACR/ACI.tf
@@ -1,12 +1,12 @@
 # Create an Azure Container Instance
 resource "azurerm_container_group" "my_app" {
-  name                = "my_app"
+  name                = "my-container-group"
   location            = azurerm_resource_group.react.location
   resource_group_name = azurerm_resource_group.react.name
   os_type             = "Linux"
 
   container {
-    name   = "my_app"
+    name   = "my-container"
     image  = "${azurerm_container_registry.myregistry.login_server}/react"  # Replace with your image reference
     cpu    = "0.5"
     memory = "1.5"
